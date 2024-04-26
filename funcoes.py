a390872439fae53c0bddf2b44cb0f0d997900a4a
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-04-26 18:49:07 -0300

    criei funcao cria_mapa

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,25 @@
 from dados import * 
 
+#Função que cria o mapa
+def cria_mapa(N): 
+    matriz = []
+    linha = [] 
+
+    i = 0 
+    while i<N:
+        linha.append(' ')
+        i+=1
+
+    i = 0
+    while i<N:
+        matriz.append(linha)
+        i+=1 
+        print(matriz)
+
+    return matriz
+
+
+
+
+
+
