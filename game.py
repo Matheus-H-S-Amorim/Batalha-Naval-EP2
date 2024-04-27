3f35cb025590a8077b7dcb24df17c775106919d1
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-04-27 19:25:23 -0300

    apagando alguns espaços

--- game.py
+++ game.py
@@ -11,14 +11,14 @@ print(" ====================================\n"
     ' =======   xxxxxxxxxxxxxxxxx   ======= \n')
 
 time.sleep(2) # espera 2 segs
+
 print('\nIniciando o Jogo!\n') 
+
 time.sleep(3)     # espera 3 seg 
 
 # Sorteando pais do Computador 
 paisSorteado = random.choice(list(PAISES))
 
-# Sorteando pais do Computador 
-paisSorteado = random.choice(list(PAISES))
 
 # Print do pais que Computador jogará com 
 print("Computador está alocando os navios de guerra do país {0}...".format(paisSorteado))
