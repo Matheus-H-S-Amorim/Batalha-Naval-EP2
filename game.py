c96f1146c09aee1f9fcd8bf61bf9858edb61b565
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-04-27 19:08:57 -0300

    mensagem inicial do jogo

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
 from dados import * 
+
+# Mensagem inicial do jogo
+print(" ====================================\n"
+    '|                                     |\n'
+    '| Bem-vindo ao INSPER - Batalha Naval |\n'
+    '|                                     |\n'
+    ' =======   xxxxxxxxxxxxxxxxx   ======= \n')
+print('\nIniciando o Jogo!\n')
