e8855b9781b88c03039fc91adfa489d06569d39f
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-04-27 19:22:19 -0300

    importei bibliotecas time e random que estavam faltando para usar o delay e para sortear o pais pro pc

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 from dados import * 
+import random 
+import time 
+
 
 # Mensagem inicial do jogo
 print(" ====================================\n"
@@ -6,12 +9,17 @@ print(" ====================================\n"
     '| Bem-vindo ao INSPER - Batalha Naval |\n'
     '|                                     |\n'
     ' =======   xxxxxxxxxxxxxxxxx   ======= \n')
-print('\nIniciando o Jogo!\n') 
 
 time.sleep(2) # espera 2 segs
-
-print('\nIniciando o Jogo!\n')   
+print('\nIniciando o Jogo!\n') 
 time.sleep(3)     # espera 3 seg 
 
 # Sorteando pais do Computador 
 paisSorteado = random.choice(list(PAISES))
+
+# Sorteando pais do Computador 
+paisSorteado = random.choice(list(PAISES))
+
+# Print do pais que Computador jogará com 
+print("Computador está alocando os navios de guerra do país {0}...".format(paisSorteado))
+print("Computador já está em posição de batalha")
