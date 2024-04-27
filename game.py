e8504e7ddea654baf3a82bd414d7dd452eeb745c
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-04-27 19:29:31 -0300

    printar dicionario de paises com suas frotas de navios na tela

--- game.py
+++ game.py
@@ -11,9 +11,7 @@ print(" ====================================\n"
     ' =======   xxxxxxxxxxxxxxxxx   ======= \n')
 
 time.sleep(2) # espera 2 segs
-
 print('\nIniciando o Jogo!\n') 
-
 time.sleep(3)     # espera 3 seg 
 
 # Sorteando pais do Computador 
@@ -23,3 +21,12 @@ paisSorteado = random.choice(list(PAISES))
 # Print do pais que Computador jogará com 
 print("Computador está alocando os navios de guerra do país {0}...".format(paisSorteado))
 print("Computador já está em posição de batalha")
+
+# Printar dicionario de paises e seus navios com delay de 1 segundo
+i = 1 
+for pais,infos in PAISES.items(): 
+    print(str(i)+":",pais)
+    for navio,blocos in infos.items():
+        print("  ",blocos,navio)
+    print("")
+    i+=1
