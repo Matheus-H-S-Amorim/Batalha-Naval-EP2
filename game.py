1ec79696bd9eb0cef4cea9c9ce3f851bbe7e6a35
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-04-27 19:11:54 -0300

    sorteio de pais do computador e pausas para fingir que pc esta pensando

--- game.py
+++ game.py
@@ -6,4 +6,12 @@ print(" ====================================\n"
     '| Bem-vindo ao INSPER - Batalha Naval |\n'
     '|                                     |\n'
     ' =======   xxxxxxxxxxxxxxxxx   ======= \n')
-print('\nIniciando o Jogo!\n')
+print('\nIniciando o Jogo!\n') 
+
+time.sleep(2) # espera 2 segs
+
+print('\nIniciando o Jogo!\n')   
+time.sleep(3)     # espera 3 seg 
+
+# Sorteando pais do Computador 
+paisSorteado = random.choice(list(PAISES))
