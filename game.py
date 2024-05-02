20c4ab4bd1667ce7066a85149feaad4f42816070
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-05-01 22:01:53 -0300

    exclui partes desnecessarias

--- game.py
+++ game.py
@@ -52,37 +52,8 @@ print("Agora é a sua vez de alocar os seus navios de guerra!")
 Mapa_PC = cria_mapa(10)
 Mapa_Jog = cria_mapa(10)
 
-#testando como vai ficar 
-blocos = [1,2,3]
-print(aloca_navios(Mapa_PC,blocos))
-print("\n")
-
-
-# Próximo navio ### fazer mostrar proximo navio 
-print('alocar: {0} ({1} blocos) '.format(x,y))
-# Próximos navios 
-print('próximos:',end="")
-lista_prox_navios = status_navios(PAISES,pais_jogador)
-for item in lista_prox_navios: 
-    print(' ',item,end=",")
 
 
 
 
 
-
-
-
-'''
-## Print pro usuario do tamanho do navio atual e navios
-x = 'torpedeiro'
-y = 2 
-
-# Próximo navio ### fazer mostrar proximo navio 
-print('alocar: {0} ({1} blocos) '.format(x,y))
-# Próximos navios 
-print('próximos:',end="")
-lista_prox_navios = status_navios(PAISES,pais_jogador)
-for item in lista_prox_navios: 
-    print(' ',item,end=",")
-'''
